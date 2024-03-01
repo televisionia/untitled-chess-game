46feddfbc2280a7be4ccd9e523d363b2b18c062d
Televisionia <106242960+televisionia@users.noreply.github.com>
2024-03-02 00:12:09 +1100

    Working on chess board initialisation

--- Board_Setup.gd
+++ Board_Setup.gd
@@ -1,9 +1,24 @@
 extends GridContainer
 
+# For identifying what sprite to render on each slot
+enum SLOT_ID {SQUARE_DARK, SQUARE_LIGHT, PIECE_PAWN, PIECE_ROOK, PIECE_KNIGHT, PIECE_BISHOP, PIECE_QUEEN, PIECE_KING}
+
+# Size of board
+var BOARD_ROWS = 8
+var BOARD_COLUMNS = 8
+
+# Holds board data for each layer (no layer limit)
+var LAYERS = []
+
+func setup_base(layer):
+	LAYERS.append([[],[]])
+	var layer_rows = LAYERS[layer][0]
+	var layer_columns = LAYERS[layer][1]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	setup_base(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
